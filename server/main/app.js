6c0ee269a2eccc48aef748b2b0e311c0a62274da
Greg Schuman <gcschuman@hotmail.com>
2020-12-28 11:25:34 -0800

    Refactored the www file.

--- server/main/app.js
+++ server/main/app.js
@@ -12,7 +12,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-module.exports = app;
-
 module.exports = app;
